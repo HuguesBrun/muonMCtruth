1b6d4cf0b3a130863659a0856d9df1465e1e38dc
Hugues Brun <hbrun@cern.ch>
2013-07-23 15:25:28 +0200

    take the same masse runge as Jan

--- computeEffTruth.C
+++ computeEffTruth.C
@@ -9,7 +9,7 @@ computeEffTruth(){
 
      chain->Add("/tmp/hbrun/tnpZ_theTreeCleaned.root");
     
-    TString denominator = "(pt>20&&tag_pt>20&&abseta<2.4&&abs(tag_eta)<2.4&&mass>80&&mass<100&&mcTrue==1)";
+    TString denominator = "(pt>20&&tag_pt>20&&abseta<2.4&&abs(tag_eta)<2.4&&mass>60&&mass<120&&mcTrue==1)";
     TString passMu17Mu8 = "((DoubleMu17Mu8_Mu17&&tag_DoubleMu17Mu8_Mu8leg)||(tag_DoubleMu17Mu8_Mu17&&DoubleMu17Mu8_Mu8leg))";
     TString passMu17TkMu8 = "((DoubleMu17TkMu8_Mu17&&tag_DoubleMu17TkMu8_TkMu8leg)||(tag_DoubleMu17TkMu8_Mu17&&DoubleMu17TkMu8_TkMu8leg))";
     
