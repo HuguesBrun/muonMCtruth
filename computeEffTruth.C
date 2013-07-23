1397489ab741160a02b4033165d2d7be6a4d434b
Hugues Brun <hbrun@cern.ch>
2013-07-23 10:29:26 +0200

    code to check the eff of mu17 in the highest eta bi

--- computeEffTruth.C
+++ computeEffTruth.C
@@ -9,23 +9,18 @@ computeEffTruth(){
 
      chain->Add("/tmp/hbrun/tnpZ_theTreeCleaned.root");
     
-    TString denominator = "(pt>20&&tag_pt>20&&abseta<2.4&&abs(tag_eta)<2.4&&mass>80&&mass<100&&mcTrue==1)";
-    TString passMu17Mu8 = "((DoubleMu17Mu8_Mu17&&tag_DoubleMu17Mu8_Mu8leg)||(tag_DoubleMu17Mu8_Mu17&&DoubleMu17Mu8_Mu8leg))";
-    TString passMu17TkMu8 = "((DoubleMu17TkMu8_Mu17&&tag_DoubleMu17TkMu8_TkMu8leg)||(tag_DoubleMu17TkMu8_Mu17&&DoubleMu17TkMu8_TkMu8leg))";
+    TString denominator = "(pt>20&&tag_pt>20&&abseta<2.4&&abs(tag_eta)<2.4&&abseta>1.8&&abs(tag_eta)>1.8&&mass>80&&mass<100&&mcTrue==1)";
+    TString passMu17Mu17 = "((Mu17||tag_Mu17))";
     
     TH1F *massDenominator = new TH1F("massDenominator","",40,60,120);
     chain->Draw("mass>>massDenominator",denominator);
 
-    TH1F *massPassingMu17Mu8 = new TH1F("massPassingMu17Mu8","",40,60,120);
-    chain->Draw("mass>>massPassingMu17Mu8",denominator+"&&"+passMu17Mu8);
-                
-    TH1F *massPassingMu17TkMu8 = new TH1F("massPassingMu17TkMu8","",40,60,120);
-    chain->Draw("mass>>massPassingMu17TkMu8",denominator+"&&"+passMu17TkMu8);
-
+    TH1F *massPassingMu17Mu17 = new TH1F("massPassingMu17Mu17","",40,60,120);
+    chain->Draw("mass>>massPassingMu17Mu17",denominator+"&&"+passMu17Mu17);
+ 
     
 
-    cout << "eff of Mu17Mu8 = " << 1.0*massPassingMu17Mu8->Integral()/massDenominator->Integral() << endl;
-    cout << "eff of Mu17TkMu8 = " << 1.0*massPassingMu17TkMu8->Integral()/massDenominator->Integral() << endl;
+    cout << "eff of Mu17Mu17 = " << 1.0*massPassingMu17Mu17->Integral()/massDenominator->Integral() << endl;
     
 
     
